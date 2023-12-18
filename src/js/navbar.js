33f77f0fdea2f9830afa1fb294145d0b556ece68
Abenezer Seifu <abenezerseifu123@gmail.com>
2023-12-18 11:49:10 +0300

    Initial Bulma stylization

---
+++ src/js/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let burger = document.querySelector('.navbar-burger');
+    let menu = document.querySelector('.navbar-menu');
+
+    burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+    });
+});
